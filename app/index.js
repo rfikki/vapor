afc873fb1f6b1cc6a212716d6f892fe724107b3e
aaron <a.aa.akio.aaron@gmail.com>
2015-02-04 22:45:23 -0800

    debug - remove noisy console logging

--- app/index.js
+++ app/index.js
@@ -8,19 +8,16 @@ const LOCAL_STORAGE_KEY = 'vapor@0'
 // load state from localStorage
 var storedState = localStorage.getItem(LOCAL_STORAGE_KEY)
 var initialState = storedState ? JSON.parse(storedState) : null
-console.log('loaded:', initialState)
 
 var app = window.app = Main(initialState)
 
 // save on throttled change event
 rafListen(app, function onChange(value) {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(value))
-  console.log('saved:', JSON.parse(JSON.stringify(value)))
 })
 
 // start app
 App(document.body, app, Main.render)
-console.log('app started.')
 
 
 // for debug
